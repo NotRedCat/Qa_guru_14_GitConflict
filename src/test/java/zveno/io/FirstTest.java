36b275643dbac457b3fb81a9e77f2586ad18945b
redcat <anastasiaredcat@gmail.com>
2022-08-10 22:43:09 +0700

    refactoring

--- src/test/java/zveno/io/FirstTest.java
+++ src/test/java/zveno/io/FirstTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class FirstTest {
     @Test
-    void firstTest(){
-        Assertions.assertTrue(3>2);
+    void firstTest() {
+        Assertions.assertFalse(3 < 2);
     }
-@Test
-    void secondTest(){
-        Assertions.assertTrue(3>1);
-}
+
+    @Test
+    void secondTest() {
+        Assertions.assertFalse(3 < 1);
+    }
 }
