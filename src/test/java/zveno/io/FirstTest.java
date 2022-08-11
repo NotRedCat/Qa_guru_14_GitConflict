c293b535d1b964fec9b7efc2ca6ef8e1886c9199
redcat <anastasiaredcat@gmail.com>
2022-08-11 23:02:24 +0700

    change tests

--- src/test/java/zveno/io/FirstTest.java
+++ src/test/java/zveno/io/FirstTest.java
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class FirstTest {
     @Test
-    void firstTest(){
-        Assertions.assertTrue(3>2);
+    void firstTest() {
+        Assertions.assertTrue(3 >= 2);
+    }
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(3 >= 1);
     }
 }
